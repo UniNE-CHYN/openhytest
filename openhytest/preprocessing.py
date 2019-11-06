042b4d823b4483d45fb48a263a9af46d1ea8f85f
Nathan Dutler <37610476+Natoliver@users.noreply.github.com>
2019-11-06 15:27:54 +0100

    added_derivatives_for_pd

--- openhytest/preprocessing.py
+++ openhytest/preprocessing.py
@@ -25,7 +25,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import openhytest as ht
-
+from scipy.interpolate import UnivariateSpline
 
 # *************************************************************
 # ---------------Preprocessing Functions:----------------------
@@ -38,15 +38,15 @@ def ldiff(data):
     data:  pandas series expects at least two traces in the dataframe.
         The first column needs to be the time.
          i.e. data.t
-        The second and following data traces needs to be sampled at the given
+        The second and following data trace needs to be sampled at the given
         time in the first column. 
             i.e. data.s, data.s2
 
     Returns
     -------
     derivative 
-        logarithmic derivative in pandas time series format with the same
-        names given by the input data
+        logarithmic derivative in pandas dataframe format with the same
+        names given by the input data.
        
     
     Examples
@@ -84,7 +84,7 @@ def ldiff_plot(data):
         
     Returns
     -------
-    plot inclusive legend 
+    plot inclusive legend
     
     Examples
     --------
@@ -100,12 +100,325 @@ def ldiff_plot(data):
     ax.set(xlabel='Time', ylabel='Drawdown and log derivative')
     
     ax.legend()
+
+
+def ldiffs(data, npoints = 20):
+    '''
+    ldiffs creates the logarithmic derivative with spline functions.
+    ------
+    data:  pandas series expects at least two traces in the dataframe.
+        The first column needs to be the time.
+         i.e. data.t
+        The second and following data trace needs to be sampled at the given
+        time in the first column. 
+            i.e. data.s, data.s2
+            
+    npoints = optional argument allowing to adjust the number of points 
+            used in the Spline
+
+    Returns
+    -------
+    derivative 
+        logarithmic derivative in pandas dataframe format with the same
+        names given by the input data.
+           
+    Examples
+    --------
+        >>> derivative = ht.ldiffs(data, [d])  
     
-    return ax
+    '''
+    df = data.head(0)
+    df = list(df)
+
+    #interpolation xi and yi
+    x = data[df[0]].to_numpy() 
+    xi = np.logspace(np.log10(x[0]), np.log10(x[len(x)-1]),  num = npoints, endpoint = True, base = 10.0, dtype = np.float64)
+    
+    for i in range(1,len(df)):   
+        #changing k & s affects the interplolation
+        spl = UnivariateSpline(x,np.array(data[df[i]].to_numpy()), k = 5, s = 0.0099)
+        yi = spl(xi)
+
+        xd = xi[1:len(xi)-1]
+        yd = xd*(yi[2:len(yi)]-yi[0:len(yi)-2])/(xi[2:len(xi)]-xi[0:len(xi)-2])       
+        
+        if i == 1:      
+            der = np.array(np.transpose([xd,yd]))
+       
+        else:
+            der = np.c_[der, np.transpose(yd)] 
+    
+    return pd.DataFrame(der, columns = df)
+
+
+def ldiffs_plot(data, npoints=20):
+    """
+    ldiffs_plot creates the plot with logarithmic derivative using slpine functions.
+    ---------
+    data : expects at least two vectors with x and y1, y2, y3,...
+ 
+    npoints = optional argument allowing to adjust the number of points 
+            used in the Spline
+            
+    Returns
+    -------
+    plot inclusive legend
+    
+    Examples
+    --------
+       >>> ht.ldiffs_plot(data,[d]])
+    """
+    
+    derivative = ht.ldiffs(data, npoints)
+    df = data.head(0)
+    df =  list(df)
+    
+    ax = data.plot(x = df[0], y=df[1:], loglog=True, marker='o', linestyle = '', colormap='jet')
+    derivative.plot(x = df[0], y=df[1:], marker='x', loglog=True, linestyle = '', colormap='jet', ax=ax, grid=True)
+    ax.set(xlabel='Time', ylabel='Drawdown and log derivative')
+    
+    ax.legend()
+    
+    
+def ldiffb(data, d = 2):
+    '''
+    ldiffb creates the logarithmic derivative with Bourdet's formula.
+    ------
+    data:  pandas series expects at least two traces in the dataframe.
+        The first column needs to be the time.
+         i.e. data.t
+        The second and following data trace needs to be sampled at the given
+        time in the first column. 
+            i.e. data.s, data.s2
+            
+    d = optional argument allowing to adjust the distance between 
+           successive points to calculate the derivative.
+
+    Returns
+    -------
+    derivative 
+        logarithmic derivative in pandas dataframe format with the same
+        names given by the input data.
+           
+    Examples
+    --------
+        >>> derivative = ht.ldiffb(data, [d])  
+    '''
+    df = data.head(0)
+    df = list(df)    
+    
+    x = data[df[0]].to_numpy()
+    logx = np.log(x)
+    dx = np.diff(logx)
+    dx1 = dx[0:len(dx)-2*d+1]
+    dx2 = dx[2*d-1:len(dx)] 
+
+    #Calculate the difference dy
+    for i in range(1, len(df)):
+        dy = np.diff(data[df[i]])
+        dy1 = dy[0:len(dx)-2*d+1]
+        dy2 = dy[2*d-1:len(dy)]
+
+        #xd and yd
+        xd = np.array(x[2:len(data[df[i]])-2]) #!!!!
+        yd = (dx2*dy1/dx1+dx1*dy2/dx2)/(dx1+dx2)
+    
+        if i == 1:      
+            der = np.array(np.transpose([xd,yd]))
+       
+        else:
+            der = np.c_[der, np.transpose(yd)] 
+    
+    return pd.DataFrame(der, columns = df)
+    
+
+def ldiffb_plot(data, d=2):
+    """
+    ldiffb_plot creates the plot with logarithmic derivative using Bourdet's  formula.
+    ---------
+    data : expects at least two vectors with x and y1, y2, y3,...
+ 
+    d = optional argument allowing to adjust the distance between 
+           successive points to calculate the derivative.
+            
+    Returns
+    -------
+    plot inclusive legend
+    
+    Examples
+    --------
+       >>> ht.ldiffb_plot(data,[d]])
+    """
+    
+    derivative = ht.ldiffb(data, d)
+    df = data.head(0)
+    df =  list(df)
+    
+    ax = data.plot(x = df[0], y=df[1:], loglog=True, marker='o', linestyle = '', colormap='jet')
+    derivative.plot(x = df[0], y=df[1:], marker='x', loglog=True, linestyle = '', colormap='jet', ax=ax, grid=True)
+    ax.set(xlabel='Time', ylabel='Drawdown and log derivative')
+    
+    ax.legend()
+    
+    
+def ldiffh(data):
+    '''
+    ldiffh creates the logarithmic derivative with Horner formula.
+    ------
+    data:  pandas series expects at least two traces in the dataframe.
+        The first column needs to be the time.
+         i.e. data.t
+        The second and following data trace needs to be sampled at the given
+        time in the first column. 
+            i.e. data.s, data.s2
+            
+    Returns
+    -------
+    derivative 
+        logarithmic derivative in pandas dataframe format with the same
+        names given by the input data.
+           
+    Examples
+    --------
+        >>> derivative = ht.ldiffh(data, [d])  
+    '''
+    
+    df = data.head(0)
+    df = list(df)
+    
+    #create the table t1,t2,t3 and s1,s2,s3
+    endt = len(data[df[0]])
+    
+    
+    x  = data[df[0]].to_numpy()
+    x1 = np.array(x[0:endt-2])
+    x2 = np.array(x[1:endt-1])
+    x3 = np.array(x[2:endt])
+    xd = x2
+    
+    for i in range(1, len(df)):
+        ends = len(data[df[i]])
+        y  = data[df[i]].to_numpy()
+        y1 = np.array(y[0:ends-2])
+        y2 = np.array(y[1:ends-1])
+        y3 = np.array(y[2:ends])
+    
+    
+        ################ to know what is what ##################
+        #d1 = (log(t2./t1).*s3)./       (log(t3./t2).*log(t3./t1));
+        #d2 = (log(t3.*t1./t2.^2).*s2)./(log(t3./t2).*log(t2./t1));
+        #d3 = (log(t3./t2).*s1)./ (log(t2./t1).*log(t3./t1));      
+        
+        #### d1 ####
+    
+        #log(t2/t1)*s3
+        D1_part1 = np.log(x2/x1) * np.array(y3)
+    
+        #log(t3/t2)*log(t3/t1)
+        D1_part2 = np.log(x3/x2)*np.log(x3/x1)
+        d1 = D1_part1/D1_part2
+    
+        #### d2 ####
+        
+        #logt3t1t2 * s2
+        D2_part1 = np.log(x3*x1/x2**2) * y2
+    
+        #log(t3/t2)*log(t2/t1)
+        D2_part2 = np.log(x3/x2)*np.log(x2/x1)
+        d2 = D2_part1 / D2_part2
+    
+        #### d3 ####
+        
+        #logt3/t2 * s1
+        D3_part1 = np.log(x3/x2) * y1
+    
+        #log(t2/t1)*log(t3/t2)
+        D3_part2 = np.log(x2/x1)*np.log(x3/x1)
+        d3 = D3_part1 / D3_part2
+    
+        yd = d1+d2-d3
+        
+        if i == 1:      
+            der = np.array(np.transpose([xd,yd]))
+       
+        else:
+            der = np.c_[der, np.transpose(yd)]         
+    
+    return pd.DataFrame(der, columns = df)
 
     
+def ldiffh_plot(data):
+    """
+    ldiffh_plot creates the plot with logarithmic derivative using Horner formula.
+    -----------
+    data : expects at least two vectors with x and y1, y2, y3,...
+ 
+
+    Returns
+    -------
+    plot inclusive legend
     
+    Examples
+    --------
+       >>> ht.ldiffh_plot(data)
+    """
     
+    derivative = ht.ldiffh(data)
+    df = data.head(0)
+    df =  list(df)
+    
+    ax = data.plot(x = df[0], y=df[1:], loglog=True, marker='o', linestyle = '', colormap='jet')
+    derivative.plot(x = df[0], y=df[1:], marker='x', loglog=True, linestyle = '', colormap='jet', ax=ax, grid=True)
+    ax.set(xlabel='Time', ylabel='Drawdown and log derivative')
+    
+    ax.legend()   
+    
+def diagnostic(data, method = 'spline'):
+    '''
+    diagnostic creates a diagnostic plot of the data
+    ----------
+    (i.e. a log-log plot of the drawdown as a function of time together with its logarithmic derivative)
+    
+    data :  pandas series expects at least two traces in the dataframe.
+        The first column needs to be the time.
+         i.e. data.t
+        The second and following data trace needs to be sampled at the given
+        time in the first column. 
+            i.e. data.s, data.s2    
+    
+    method :    optional argument allowing to select different methods of 
+        computation of the derivative
+    'spline' for spline resampling
+    in that case d is the number of points for the spline
+    
+    'direct' for direct derivation
+    in that case the value provided in the variable d is not used
+    
+    'bourdet' for the Bourdet et al. formula
+    in that case d is the lag distance used to compute the derivative
+    
+    'horner'  for the logarithmic derivative with Horne formula
+    
+    Description:
+    This function allows to create rapidly a diagnostic plot
+    (i.e. a log-log plot of the drawdown as a function of time together with its logarithmic derivative) of the data. 
+    Example: 
+        diagnostic(data) 
+        diagnostic(data,'horner')
+
+        '''
+
+    if method == 'spline':
+        ldiffs_plot(data)
+    elif method == 'direct' : 
+        ldiff_plot(data)
+    elif method == 'bourdet':
+        ldiffb_plot(data)
+    elif method == 'horner':
+        ldiffh_plot(data)
+    else : 
+        print('ERROR: diagnostic(data,method)')
+        print(' The method selected for log-derivative calculation is unknown')   
     
 
 
